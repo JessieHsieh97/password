32617b17fb7d0a8ffd6241bc886844f58c5e099b
Jessie Hsieh <jessiehsieh86@gmail.com>
2022-08-23 15:34:10 +0800

    change to while i>0

--- password.py
+++ password.py
@@ -1,25 +1,11 @@
 password = 'a123456'
-i = 3
-while True:
+i = 3 #i代表剩餘機會
+while i > 0:
 	question = input('Please insert password:')
 	if question == password:
 		print('Succesfully login')
 		break
 	else:
 		i = i - 1
-		print('Wrong password! ',i ,'time left') #i代表剩餘機會
-		if i == 0:
-			break
-
-
-
+		print('Wrong password! ',i ,'time left') 
 		
-
-
-	
-	
-	
-
-	    
-
-
