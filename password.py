db23993787c9236de95518ab59cbf24b3640e200
Jessie Hsieh <jessiehsieh86@gmail.com>
2022-08-23 15:45:24 +0800

    不顯示0次機會

--- password.py
+++ password.py
@@ -1,11 +1,13 @@
 password = 'a123456'
 i = 3 #i代表剩餘機會
 while i > 0:
+	i = i - 1
 	question = input('Please insert password:')
 	if question == password:
 		print('Succesfully login')
 		break
 	else:
-		i = i - 1
-		print('Wrong password! ',i ,'time left') 
+		print('Wrong password!') 
+		if i > 0:
+			print( i ,'time left')
 		
